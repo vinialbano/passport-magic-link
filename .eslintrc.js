5489dc6c63946ebfc255cf455b5f912b4b420921
Vinicius Albano <vinialbano@gmail.com>
2018-08-22 18:37:13 -0300

    Add development tools

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": ["plugin:jest/recommended", "standard"],
+    "plugins": ["jest"],
+    "env": {
+        "node": true,
+        "es6": true,
+        "jest": true
+    }
+};
